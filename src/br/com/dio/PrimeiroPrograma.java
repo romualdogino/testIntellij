134dc399b6369c9e3de7151f97441636e7bd6d45
romualdogino <romualdo.gino@gmail.com>
2022-07-31 08:55:57 -0300

    primeiro commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,13 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Gato g1 = new Gato("nome1", "azul", 2);
+        Gato g2 = new Gato("nome1", "azul", 2);
+
+        System.out.println(g1);
+//        System.out.println("Hello world");
+    }
+}
